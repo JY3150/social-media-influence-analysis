62efab0f6e309082eb3554ed0184c3ea510aacb8
Tianyi Pan <nappan363@gmail.com>
2023-07-18 10:44:43 -0400

    simplify internal implementation

--- src/TS/TSATool.py
+++ src/TS/TSATool.py
@@ -8,25 +8,27 @@ def identity(x: Sequence) -> Sequence:
 
 def sma(x: Sequence, window: int) -> Sequence:
     # Note that <window> should be an odd number
-    if not isinstance(window, int) or window % 2 != 1 or window < 0:
-        raise ValueError("Window size should be positive odd integer")
-
-    seq_len = len(x)
-    ma = [0] * seq_len
-
-    if len(x) <= window:
-        raise ValueError("window size too large")
-
-    half_w = window // 2
-    for i in range(seq_len):
-        if i < half_w:
-            target_list = x[:i + half_w + 1] + [x[0]] * (window - i - half_w - 1)
-        elif i > seq_len - half_w - 1:
-            target_list = x[i - half_w:] + [x[-1]] * (window - seq_len + i - half_w)
-        else:
-            target_list = x[i - half_w: i + half_w + 1]
-        assert len(target_list) == window
-        ma[i] = np.average(target_list)
+    # if not isinstance(window, int) or window % 2 != 1 or window < 0:
+    #     raise ValueError("Window size should be positive odd integer")
+    #
+    # seq_len = len(x)
+    # ma = [0] * seq_len
+    #
+    # if len(x) <= window:
+    #     raise ValueError("window size too large")
+    #
+    # half_w = window // 2
+    # for i in range(seq_len):
+    #     if i < half_w:
+    #         target_list = x[:i + half_w + 1] + [x[0]] * (window - i - half_w - 1)
+    #     elif i > seq_len - half_w - 1:
+    #         target_list = x[i - half_w:] + [x[-1]] * (window - seq_len + i - half_w)
+    #     else:
+    #         target_list = x[i - half_w: i + half_w + 1]
+    #     assert len(target_list) == window
+    #     ma[i] = np.average(target_list)
+    weight = [1 / window] * window
+    ma = np.convolve(x, weight, "valid")
     return ma
 
 
